b17c8cdb8b84600f22cbfe3b50f557d5e4ba5823
David Mezza <d.mezza@zunocarbon.com>
2023-05-17 17:42:22 +0300

    :white_check_mark: Done with the middle sidebar container

--- lib/widgets/sidebar/middle/middle_sidebar.dart
+++ lib/widgets/sidebar/middle/middle_sidebar.dart
@@ -27,10 +27,118 @@ class MiddleSidebarContainer extends StatelessWidget {
             width: 2.0,
           ),
         ),
-        child: const Text(
-          "Middle",
-          style: TextStyle(
-            color: kTextMinorColor,
+        child: MiddleContainer(),
+      ),
+    );
+  }
+}
+
+class MiddleContainer extends StatelessWidget {
+  const MiddleContainer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        // Row of "Your Library"
+        Padding(
+          padding: EdgeInsets.all(12.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Icon(
+                    Icons.home,
+                    color: kIconNotSelected,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    "Your Library",
+                    style: TextStyle(
+                      color: kTextMinorColor,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.add,
+                    color: kIconNotSelected,
+                  ),
+                  SizedBox(width: 10),
+                  Icon(
+                    Icons.arrow_forward,
+                    color: kIconNotSelected,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+
+        // Row of circular containers
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              SizedBox(width: 12),
+              CircularContainerMiddleSidebar(
+                lebo: "Playlists",
+              ),
+              SizedBox(width: 12),
+              CircularContainerMiddleSidebar(
+                lebo: "Artists",
+              ),
+              SizedBox(width: 12),
+              CircularContainerMiddleSidebar(
+                lebo: "Albums",
+              ),
+              SizedBox(width: 12),
+              CircularContainerMiddleSidebar(
+                lebo: "Podcasts",
+              ),
+              SizedBox(width: 12),
+              CircularContainerMiddleSidebar(
+                lebo: "Motivation",
+              ),
+              SizedBox(width: 12),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class CircularContainerMiddleSidebar extends StatelessWidget {
+  const CircularContainerMiddleSidebar({
+    super.key,
+    required this.lebo,
+  });
+
+  final String lebo;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        color: kCircularContainerColor,
+        border: Border.all(
+          color: kColumnCardsColor,
+          width: 1.0,
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          lebo,
+          style: const TextStyle(
+            color: kTextColor,
           ),
         ),
       ),
